ce1d088919868dad125cf7ea917d8a8a96b30111
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-06-06 17:01:51 +0900

    refactor: device register resource in Application API

--- server/api/app/resources/register.py
+++ server/api/app/resources/register.py
@@ -3,12 +3,39 @@ from server.api.app.models.register import RegisterRequestModel
 from sanic.exceptions import abort
 from sanic.response import json as res_json
 from sanic_openapi import doc
-from bson import ObjectId
+import time
 
 @app_api.post('/register')
 @doc.summary('Register device')
 @doc.consumes(RegisterRequestModel, content_type='application/json', location='body')
 @doc.response(200, None, description='Success')
-@doc.response(500, None, description='Error while execution')
+@doc.response(200, None, description='Not valid wallet address')
+@doc.response(500, None, description='Error while registration')
 async def register(request):
-    return res_json({})
+    wallet = request.json['wallet']
+    if len(wallet) != 42:
+        abort(404)
+    
+    # register device
+    device = {
+        'name': request.json['name'],
+        'wallet': wallet,
+        'update': None
+    }
+    res = await request.app.db.devices.insert_one(device)
+    if not res.acknowledged:
+        abort(500)
+    
+    # save log
+    log = {
+        'type': 'register',
+        'timestamp': int(time.time()),
+        'json': {
+            'name': device['name'],
+            'wallet': device['wallet']
+        }
+    }
+    await request.app.db.devices.insert_one(device)
+    # error handling is not necessary for logging
+
+    return res_json({}) # 200
