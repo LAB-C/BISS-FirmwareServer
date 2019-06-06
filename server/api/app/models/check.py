a4638590b548e1ffed7a3a769242775b606d8adf
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-06-06 17:14:07 +0900

    refactor: check device existence resource in Application API

--- server/api/app/models/check.py
+++ server/api/app/models/check.py
@@ -1,7 +1,4 @@
 from sanic_openapi import doc
 
-class CheckRequestModel:
-    wallet = doc.String('Device wallet address', required=True)
-
 class CheckResponseModel:
     exist = doc.Boolean('Device register status')
--- server/api/app/resources/check.py
+++ server/api/app/resources/check.py
@@ -1,17 +1,22 @@
 from server.api.app import app_api
-from server.api.app.models.check import CheckRequestModel, CheckResponseModel
+from server.api.app.models.check import CheckResponseModel
 from sanic.exceptions import abort
 from sanic.response import json as res_json
 from sanic_openapi import doc
 from bson import ObjectId
 
-@app_api.post('/check')
+@app_api.get('/check/<wallet:string>')
 @doc.summary('Check device registeration')
-@doc.consumes(CheckRequestModel, content_type='application/json', location='body')
 @doc.produces(CheckResponseModel, content_type='application/json', description='Successful')
 @doc.response(200, None, description='Success')
 @doc.response(500, None, description='Error while execution')
-async def check(request):
-    return res_json({
-      'exist': True
-    })
+async def check(request, wallet):
+    try:
+        device = await request.app.db.devices.find_one({
+            'wallet': wallet
+        })
+        return res_json({
+            'exist': bool(device)
+        })
+    except:
+        abort(500)
