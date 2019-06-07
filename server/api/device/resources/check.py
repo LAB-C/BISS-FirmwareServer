bc321cb29d25cadacf9d0d153bb3d85811d17997
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-06-07 10:08:42 +0900

    refactor: update hash check resource in Device API

--- server/api/device/resources/check.py
+++ server/api/device/resources/check.py
@@ -8,6 +8,7 @@ from sanic.exceptions import abort
 from sanic.response import json as res_json
 from sanic_openapi import doc
 from bson import ObjectId
+import time
 
 @device_api.post('/check/update')
 @doc.summary('Check update for device')
@@ -17,13 +18,14 @@ from bson import ObjectId
 @doc.response(404, None, description='No such device')
 @doc.response(500, None, description='Error while execution or no update found')
 async def check_update(request):
-    wallet = request.json['wallet']
+    # find device with given wallet
     device = await request.app.db.devices.find_one({
-        'wallet': wallet
+        'wallet': request.json['wallet']
     })
     if not device:
         abort(404)
 
+    # return update info if available
     if device['update']:
         update = await request.app.db.updates.find_one({
             '_id': ObjectId(device['update'])
@@ -44,5 +46,36 @@ async def check_update(request):
 @doc.consumes(HashRequestModel, content_type='application/json', location='body')
 @doc.response(200, None, description='Success')
 @doc.response(500, None, description='Error while execution')
-async def check_hash(request):
+async def check_hash(request, file_id):
+    # find update with given fileId
+    update = await request.app.db.updates.find_one({
+        'fileId': ObjectId(file_id)
+    })
+    if not update:
+        abort(404)
+
+    # validation failed
+    if update.hash != request.json['hash']:
+        abort(400)
+
+    # update device status
+    res = await request.app.db.devices.update({
+        'wallet': request.json['wallet']
+    }, {
+        '$set': { 'update': None }
+    })
+    if not res.acknowledged:
+        abort(404)
+
+    # log success
+    log = {
+        'type': 'success',
+        'timestamp': int(time.time()),
+        'json': {
+            'route': update['route'],
+            'hash': update['hash']
+        }
+    }
+    await request.app.db.logs.insert_one(log)
+    
     return res_json({})
